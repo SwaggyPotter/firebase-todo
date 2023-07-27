38650fb17aa09b97cc35855fc9f15c618a1af7fe
Tim <tim.spiele1@freenet.de>
2023-07-27 21:09:20 +0200

    set up firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'myfirebasetodo-394118',
+    appId: '1:135276742611:web:9024df4fd55e7adc9e9126',
+    storageBucket: 'myfirebasetodo-394118.appspot.com',
+    apiKey: 'AIzaSyBiofjulab74i9uCG1Sp9pbdVJDDmKnFy4',
+    authDomain: 'myfirebasetodo-394118.firebaseapp.com',
+    messagingSenderId: '135276742611',
+    measurementId: 'G-ZX3XWVL2CK',
+  },
+};
