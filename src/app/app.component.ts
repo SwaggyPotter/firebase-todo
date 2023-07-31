715aa0e27eee446aa7a22949e38f25604b2403d6
Tim <tim.spiele1@freenet.de>
2023-07-31 17:08:08 +0200

    init angular fire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection, deleteDoc } from '@angular/fire/firestore';
+import { doc, setDoc } from '@firebase/firestore';
+import { Observable } from 'rxjs';
+
+interface Item {
+  name: string,
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase-todo';
+  todos$: Observable<any[]>;
+  firestore: Firestore = inject(Firestore);
+  todoText: string = '';
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'todos');
+    this.todos$ = collectionData(itemCollection);
+
+    this.todos$.subscribe((newtodos) => {
+      console.log('Neue todos:', newtodos)
+    })
+  }
+
+  async addTodo() {
+    const itemCollection = collection(this.firestore, 'todos');
+    await setDoc(doc(itemCollection, this.todoText), { name: this.todoText });
+  }
+
+  async deleteTodo(todoName: any) {
+    const itemCollection = collection(this.firestore, 'todos');
+    await deleteDoc(doc(itemCollection, todoName));
+  }
 }
